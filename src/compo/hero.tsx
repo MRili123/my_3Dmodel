d8e1b7d539ba6ffbcb2dae4eb74d25e28e5b8a22
ILIAS JEBRANE <110349305+MRili123@users.noreply.github.com>
2026-02-01 19:16:49 +0100

    Update proposal text in hero component
    huda

--- src/compo/hero.tsx
+++ src/compo/hero.tsx
@@ -152,7 +152,7 @@ export default function ThreeModel() {
           margin: 0,
           textShadow: '0 2px 10px rgba(59, 130, 246, 0.3)'
         }}>
-          Would You marry Me? 🤔
+         Huda will u be my valentine ? 
         </h1>
       </div>
       
